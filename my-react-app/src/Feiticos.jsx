a15e92a4feac3868c4dac295b28a68c4857af129
FelipeAnversa <jdanversa@gmail.com>
2025-12-02 15:38:52 -0300

    💎 Implementação de createTheme e ThemeProvider no código.

--- my-react-app/src/Feiticos.jsx
+++ my-react-app/src/Feiticos.jsx
@@ -6,6 +6,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { Typography } from '@mui/material';
+import { createTheme , ThemeProvider } from '@mui/material';
 
 export default function Feiticos () {
     const [feiticos, setFeiticos] = useState(null);
@@ -20,52 +21,61 @@ export default function Feiticos () {
         setSelecionado(feiticoSelecionado);
     };
 
+    const theme = createTheme({
+        palette: {
+            custon: {
+                red: '#f00',
+            },
+        },
+    });
 
     return (
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth margin='0 0 20px 0'>
-                <InputLabel id='label'>Name</InputLabel>
-                <Select 
-                    labelId='label'
-                    id='select'
-                    label='Name'
-                    onChange={selecionar}
-                >
-                    <MenuItem value="">Select a option:</MenuItem>
-                    {feiticos && feiticos.map(feitico => (
-                        <MenuItem key={feitico.id} value={feitico.name}>
-                            {`${feitico.name}`}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <br />
-            {selecionado && (
-                <div>
-                    <Typography 
-                        marginTop={5}
-                        variant="h5" 
-                        component="div"
-                        fontWeight= 'fontWeightBold'
-                        sx={{ fontFamily: 'Arial', fontSize: 50 }}
+        <ThemeProvider theme={theme}>
+            <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth margin='0 0 20px 0'>
+                    <InputLabel id='label'>Name</InputLabel>
+                    <Select 
+                        labelId='label'
+                        id='select'
+                        label='Name'
+                        onChange={selecionar}
                     >
-                        Description:
-                    </Typography>
-                    <Typography 
-                        variant="h5" 
-                        component="div"
-                        fontWeight= 'fontWeightBold'
-                        sx={{ 
-                        fontFamily: 'Arial',
-                        fontSize: 50, 
-                        color: 'rgb(236, 0, 0)'
-                        }}
-                    >
-                        {selecionado.description}
-                    </Typography>
-                </div>
-            )}
-        </Box>
+                        <MenuItem value="">Select a option:</MenuItem>
+                        {feiticos && feiticos.map(feitico => (
+                            <MenuItem key={feitico.id} value={feitico.name}>
+                                {`${feitico.name}`}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <br />
+                {selecionado && (
+                    <div>
+                        <Typography 
+                            marginTop={5}
+                            variant="h5" 
+                            component="div"
+                            fontWeight= 'fontWeightBold'
+                            sx={{ fontFamily: 'Arial', fontSize: 50 }}
+                        >
+                            Description:
+                        </Typography>
+                        <Typography 
+                            variant="h5" 
+                            component="div"
+                            fontWeight= 'fontWeightBold'
+                            sx={{ 
+                                fontFamily: 'Arial',
+                                fontSize: 50, 
+                                color: 'custon.red'
+                            }}
+                        >
+                            {selecionado.description}
+                        </Typography>
+                    </div>
+                )}
+            </Box>
+        </ThemeProvider>
     );
 
     async function pegarFeiticos() {
